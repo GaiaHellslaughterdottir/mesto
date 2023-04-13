26705a8db6ac6c221995b32b42b6629ac406af2b
GaiaHellslaughterdottir <hellslaughterdottir@ya.ru>
2023-04-13 19:44:51 +0300

    13.04 19:44

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@ let popUp = document.querySelector('#popup');
 
 let formElement = document.querySelector('#popup__form');
 
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseIcon = document.querySelector('.popup__close-icon');
+
 
 let popupFieldName = document.querySelector('#popup__form-field-name');
 let popupFieldVocation = document.querySelector('#popup__form-field-vocation');
@@ -26,21 +29,7 @@ function handleFormSubmit(evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+profileEditButton.addEventListener('click', openPopUp);
+popupCloseIcon.addEventListener('click', closePopUp);
 
 
-
-
-/*
-function openPopUp() {
-  formElement.classList.add('popup_opened');
-}
-
-function closePopUp() {
-  formElement.classList.remove('popup_opened');
-}
-
-function handleFormSubmit() {
-  profileInfoName.textContent = popupFieldName.value;
-  profileInfoVocation.textContent = popupFieldVocation.value;
-  closePopUp();
-}*/
