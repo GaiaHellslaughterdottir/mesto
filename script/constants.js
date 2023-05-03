633f74c06d3ba59fedf59dfa3e564721cfe24578
GaiaHellslaughterdottir <hellslaughterdottir@ya.ru>
2023-05-03 12:23:20 +0300

    03.05 12:22

---
+++ script/constants.js
@@ -0,0 +1,64 @@
+//Всплывающее окно редактирования профиля
+const profilePopup = document.querySelector('#profile-popup');
+//Всплывающее окно добавления места
+const placePopup = document.querySelector('#place-popup');
+//Всплывающее окно изображения места
+const placeImagePopup = document.querySelector('#place-image-popup');
+//Иконки закрытия всплывающих окон
+const popupCloseIconList = document.querySelectorAll('.popup__close-icon');
+
+//Поле формы для имени в профиле
+const formFieldName = document.querySelector('#form__field-name');
+//Поле формы для профессии в профиле
+const formFieldVocation = document.querySelector('#form__field-vocation');
+//Имя в профиле
+const profileInfoName = document.querySelector('.profile__info-name');
+//Профессия в профиле
+const profileInfoVocation = document.querySelector('.profile__info-vocation');
+//Кнопка редактирования профиля
+const profileEditButton = document.querySelector('.profile__edit-button');
+//Форма редактирования профиля
+const profilePopupForm = profilePopup.querySelector('.form');
+
+//Кнопка создания новой карточки места
+const profileAddPlaceButton = document.querySelector('.profile__add-place-button');
+//Секция с карточками
+const sectionPlaces = document.querySelector('.elements');
+//Поле формы для названия места
+const placeFieldName = document.querySelector('#form__field-place-name');
+//Поле формы для изображения места
+const placeFieldImage = document.querySelector('#form__field-place-image');
+//Название места
+const placeInfoImage = document.querySelector('.place-info__image');
+//Изображение места
+const placeInfoTitle = document.querySelector('.place-info__title');
+//Форма создания карточки места
+const placePopupForm =  placePopup.querySelector('.form');
+
+//Массив с содержимым галереи
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
--- script/script.js
+++ script/script.js
@@ -1,37 +1,9 @@
-//Всплывающее окно редактирования профиля
-const profilePopUp = document.querySelector('#profile-popup');
-//Всплывающее окно добавления места
-const placePopUp = document.querySelector('#place-popup');
-//Всплывающее окно изображения места
-const placeImagePopup = document.querySelector('#place-image-popup');
-
-//Поле формы для имени в профиле
-const formFieldName = document.querySelector('#form__field-name');
-//Поле формы для профессии в профиле
-const formFieldVocation = document.querySelector('#form__field-vocation');
-//Имя в профиле
-const profileInfoName = document.querySelector('.profile__info-name');
-//Профессия в профиле
-const profileInfoVocation = document.querySelector('.profile__info-vocation');
-
-//Секция с карточками
-const sectionPlaces = document.querySelector('.elements');
-//Поле формы для названия места
-const placeFieldName = document.querySelector('#form__field-place-name');
-//Поле формы для изображения места
-const placeFieldImage = document.querySelector('#form__field-place-image');
-//Название места
-const placeInfoImage = document.querySelector('.place-info__image');
-//Изображение места
-const placeInfoTitle = document.querySelector('.place-info__title');
-
 /**
  * Открытие всплывающего окна
  * @param popup - вслывающее окно
  */
 function openPopup(popup) {
-  popup.classList.add('popup_visible');
-  popup.classList.remove('popup_invisible');
+  popup.classList.add('popup_opened');
 }
 
 /**
@@ -39,8 +11,7 @@ function openPopup(popup) {
  * @param popup - всплывающее окно
  */
 function closePopup(popup) {
-  popup.classList.add('popup_invisible');
-  popup.classList.remove('popup_visible');
+  popup.classList.remove('popup_opened');
 }
 
 /**
@@ -49,16 +20,15 @@ function closePopup(popup) {
 function handleOpenProfilePopup() {
   formFieldName.value = profileInfoName.textContent;
   formFieldVocation.value = profileInfoVocation.textContent;
-  openPopup(profilePopUp);
+  openPopup(profilePopup);
 }
 
 /**
  * Обработчик события открытия окна добавления места
  */
 function handlerOpenPlaceEditPopup() {
-  placeFieldName.value = '';
-  placeFieldImage.value = '';
-  openPopup(placePopUp);
+  placeFieldName.closest('.form').reset();
+  openPopup(placePopup);
 }
 
 /**
@@ -68,6 +38,7 @@ function handlerOpenPlaceEditPopup() {
 function handleOpenImagePopup(evt) {
   evt.preventDefault();
   placeInfoImage.src = evt.target.src;
+  placeInfoImage.alt = evt.target.alt;
   placeInfoTitle.textContent = evt.target.closest('.element').querySelector('.element__place').textContent;
   openPopup(placeImagePopup);
 }
@@ -89,7 +60,7 @@ function handleFormProfileSubmit(evt) {
   evt.preventDefault();
   profileInfoName.textContent = formFieldName.value;
   profileInfoVocation.textContent = formFieldVocation.value;
-  closePopup(profilePopUp);
+  closePopup(profilePopup);
 }
 
 /**
@@ -98,8 +69,9 @@ function handleFormProfileSubmit(evt) {
  */
 function handleFormPlaceSubmit(evt) {
   evt.preventDefault();
-  addCard(placeFieldName.value, placeFieldImage.value);
-  closePopup(placePopUp);
+  const cardElement = createCard(placeFieldName.value, placeFieldImage.value);
+  addCard(cardElement);
+  closePopup(placePopup);
 }
 
 /**
@@ -114,17 +86,26 @@ function handleToggleLike(evt) {
 const cardTemplate = document.querySelector('#card-template').content;
 
 /**
- * Функция добавления новой карточки в галерею
+ * Функция создания новой карточки
  * @param name - наименование места
  * @param link - ссылка на изображение места
  */
-function addCard(name, link) {
+function createCard(name, link) {
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = cardElement.querySelector('.element__image');
   cardElement.querySelector('.element__place').textContent = name;
-  cardElement.querySelector('.element__image').src = link;
+  elementImage.src = link;
+  elementImage.addEventListener('click', handleOpenImagePopup);
   cardElement.querySelector('.element__like').addEventListener('click', handleToggleLike);
   cardElement.querySelector('.element__basket').addEventListener('click', handleRemoveCard);
-  cardElement.querySelector('.element__image').addEventListener('click', handleOpenImagePopup);
+  return cardElement;
+}
+
+/**
+ * Функция добавления новой карточки в галерею
+ * @param cardElement - добавляемая карточка
+ */
+function addCard(cardElement) {
   sectionPlaces.prepend(cardElement);
 }
 
@@ -136,50 +117,23 @@ function handleRemoveCard(evt) {
   evt.target.closest('.element').remove();
 }
 
-//Массив с содержимым галереи
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 /**
  * Инициализация сайта
  */
 function init() {
-  profilePopUp.querySelector('.form').addEventListener('submit', handleFormProfileSubmit);
-  placePopUp.querySelector('.form').addEventListener('submit', handleFormPlaceSubmit);
+  profilePopupForm.addEventListener('submit', handleFormProfileSubmit);
+  placePopupForm.addEventListener('submit', handleFormPlaceSubmit);
 
-  document.querySelector('.profile__edit-button').addEventListener('click', handleOpenProfilePopup);
-  document.querySelector('.profile__add-place-button').addEventListener('click', handlerOpenPlaceEditPopup);
+  profileEditButton.addEventListener('click', handleOpenProfilePopup);
+  profileAddPlaceButton.addEventListener('click', handlerOpenPlaceEditPopup);
 
-  document.querySelectorAll('.popup__close-icon').forEach(closeIcon => {
+  popupCloseIconList.forEach(closeIcon => {
     closeIcon.addEventListener('click', handleClosePopup);
   });
 
   initialCards.forEach(card => {
-    addCard(card.name, card.link);
+    const cardElement = createCard(card.name, card.link);
+    addCard(cardElement);
   })
 }
 
